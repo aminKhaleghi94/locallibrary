ed66854157fa9761c718c42e4ae740f80bfbb11a
aminkhaleghi94 <aminkhaleghi1373@gmail.com>
2019-12-28 22:05:54 +0330

    cm

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^catalog/', include('catalog.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+]
+
